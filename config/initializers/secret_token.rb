c6473f0d3d5e3def55abad24a8c9a3c9991b7527
Austin J. Pocus <ajpocus@gmail.com>
2012-03-27 21:33:04 -0700

    Initial commit, gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saygepoints::Application.config.secret_token = 'ca828f0d68f2e0648d25549d31908900ef0333f0ce92262396d844839c6dd7d399d5a05423315dbc5fc467cd8ef5d068e814398bcef9fd70e71313bbe1066c97'
