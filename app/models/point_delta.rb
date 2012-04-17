b91e2370c65ac043ff63936b4485b10455766b54
Austin J. Pocus <ajpocus@gmail.com>
2012-04-16 23:40:10 -0700

    Modify PointBalance after PointDelta creation

--- app/models/point_delta.rb
+++ app/models/point_delta.rb
@@ -2,6 +2,22 @@ class PointDelta < ActiveRecord::Base
   belongs_to :from_user, :class_name => 'User'
   belongs_to :to_user, :class_name => 'User'
   belongs_to :point_type
+  
+  after_create :add_to_balance
+  
+  def add_to_balance
+    balance = PointBalance.where(:user_id => self.to_user.id,
+      :point_type_id => self.point_type.id)
+    if balance.empty?
+      balance = PointBalance.create(:user_id => self.to_user.id,
+        :point_type_id => self.point_type.id)
+    else
+      balance = balance.first
+    end
+    
+    balance.amount += self.amount
+    balance.save
+  end
 end
 # == Schema Information
 #
