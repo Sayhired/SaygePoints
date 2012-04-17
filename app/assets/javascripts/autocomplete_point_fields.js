611da30b54d2467f796ef54b4a2cee32aef4b5f7
Austin J. Pocus <ajpocus@gmail.com>
2012-04-16 23:16:57 -0700

    Autofocus on autocomplete, down arrow to see types

--- app/assets/javascripts/autocomplete_point_fields.js
+++ app/assets/javascripts/autocomplete_point_fields.js
@@ -1,9 +1,13 @@
 $(function () {
   $.get("/point_types.json", function (typeList) {
-    $("#point_type").autocomplete({ source: typeList, delay: 0 });
+    $("#point_type").autocomplete({
+      source: typeList, delay: 0, minLength: 0, autoFocus: true
+    });
   });
   
   $.get("/sayges", function (saygeList) {
-    $("#to_user").autocomplete({ source: saygeList, delay: 0 });
+    $("#to_user").autocomplete({
+      source: saygeList, delay: 0, autoFocus: true
+    });
   });
 });
