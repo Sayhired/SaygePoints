930b91177cfb2f96d8285740aad80c49076f9115
Austin J. Pocus <ajpocus@gmail.com>
2012-04-16 23:51:28 -0700

    Close #5: v1 of sayges page, show point totals

--- app/controllers/sayges_controller.rb
+++ app/controllers/sayges_controller.rb
@@ -1,6 +1,7 @@
 class SaygesController < ApplicationController
   def index
     respond_to do |format|
+      format.html
       format.json { render :json => User.all.map(&:name).to_json }
     end
   end
